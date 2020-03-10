57b6984e57c6369e46073fd237194bb52d93e4f5
Morpilou <Morpilou@users.noreply.github.com>
2020-03-10 14:59:39 +0100

    fix modal and code cleared
    Index : Code cleared
    
    Gallery : Fix modal that doesn't display

--- js/gallery.js
+++ js/gallery.js
@@ -1,22 +1,21 @@
-
 $('.gallery div.item a').click(function() {
-    var itemID = $(this).attr('href');
-    $('.gallery div.item').addClass('item_open');
-    $(itemID).addClass('item_open');
-    return false;
+  var itemID = $(this).attr('href');
+  $('.gallery div.item').addClass('item_open');
+  $(itemID).addClass('item_open');
+  return false;
 });
 
 $('.close').click(function() {
-    $('.port, .gallery div.item').removeClass('item_open');
-    return false;
+  $('.port, .gallery div.item').removeClass('item_open');
+  return false;
 });
 $('.close-btn').click(function() {
-    $('.port, .gallery div.item').removeClass('item_open');
-    return false;
+  $('.port, .gallery div.item').removeClass('item_open');
+  return false;
 });
 
 $(".gallery div.item a").click(function() {
-    $('html, body').animate({
-        scrollTop: parseInt($("#top").offset().top)
-    }, 400);
+  $('html, body').animate({
+    scrollTop: parseInt($("#top").offset().top)
+  }, 400);
 });
