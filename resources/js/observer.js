49e058f9b812f14acfad0a55ecd90cf10106fdca
Morpilou <Morpilou@users.noreply.github.com>
2020-03-25 10:33:54 +0100

    use replace instead of "add" and "remove"

--- resources/js/observer.js
+++ resources/js/observer.js
@@ -1,6 +1,5 @@
 const logo = document.querySelector(".logo-top");
-const masthead = document.querySelector("header");
-const section = document.querySelector("section");
+const masthead = document.querySelector(".masthead");
 
 const options = {
   rootMargin: "-75px 0px 0px 0px"
@@ -9,12 +8,10 @@ const options = {
 const projectObserver = new IntersectionObserver(function(entries, options) {
     entries.forEach(entry => {
       if (!entry.isIntersecting) {
-        logo.classList.add("logo-dark");
-        logo.classList.remove("logo-light");
+        logo.classList.replace("logo-light", "logo-dark");
 
       } else {
-        logo.classList.remove("logo-dark");
-        logo.classList.add("logo-light");
+        logo.classList.replace("logo-dark", "logo-light");
 
       }
     });
@@ -22,19 +19,3 @@ const projectObserver = new IntersectionObserver(function(entries, options) {
   options);
 
 projectObserver.observe(masthead);
-
-const aboutObserver = new IntersectionObserver(function(entries, options) {
-    entries.forEach(entry => {
-      if (!entry.isIntersecting) {
-        logo.classList.add("logo-light");
-        logo.classList.remove("logo-dark");
-      } else {
-        logo.classList.add("logo-dark");
-        logo.classList.remove("logo-light");
-
-      }
-    });
-  },
-  options);
-
-aboutObserver.observe(section);
